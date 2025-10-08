cd94355dd58219ddcab49699ea7f1fb5a5c86c43
grazy2607f <batista.grazielly@escola.pr.gov.br>
2025-10-08 08:14:47 -0300

    ok

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-let titulo = document
+let titulo = document.querySelector('h1')
+titulo.innerHTML = 'jogo do número secreto';
+
+let paragrafo = document.querySelector('p')
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10';  
