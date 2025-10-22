f62409dddf7833b58a9157ce2d6e9a3f4fc1a5cb
grazy2607f <batista.grazielly@escola.pr.gov.br>
2025-10-22 08:17:14 -0300

    ok

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-let numeroSecreto = gerarNumeroALeatorio();
+let listaDeNúmerosSorteados = [];
+let númerolimite = 10;
+let númeroSecreto = gerarNumeroALeatorio();
 let tentativas = 1;
-
+ 
 function exibirtextoNaTela(tag, texto) {
-let campo = document.querySelector(tag);
-campo.innerHTML = texto;
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'brazilian portuguese female', {rate:1.2}).  
 }
 
+function exibirMensagemInicial
 exibirtextoNaTela('h1', 'jogo o número secreto');
 exibirtextoNaTela('p','escolha um número entre 1 e 10');
 
@@ -13,6 +17,11 @@ exibirtextoNaTela('p','escolha um número entre 1 e 10');
 function verificarChute(){
 let chute = Document.querySelector('input').value;
  
+
+
+
+
+
 if (chute == numeroSecreto) {
 exibirTextoNaTela('h1', 'Acertou!');
 exibirtextoNaTela('p', 'você descobriu o numero secreto com 5 tentativas!')
